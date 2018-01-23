5ce405071ac240e0ab9acff062c1fc557873517b
mhq199657 <mhq199657@163.com>
2018-01-23 18:00:35 +0800

    Extract `args.length`

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -258,16 +258,17 @@ public class AddressBook {
      * @param args full program arguments passed to application main method
      */
     private static void processProgramArgs(String[] args) {
-        if (args.length >= 2) {
+        int length = args.length;
+        if (length >= 2) {
             showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
             exitProgram();
         }
 
-        if (args.length == 1) {
+        if (length == 1) {
             setupGivenFileForStorage(args[0]);
         }
 
-        if(args.length == 0) {
+        if(length == 0) {
             setupDefaultFileForStorage();
         }
     }
